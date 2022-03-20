9e737a2e7d345ad6540276a6455fc219bfd5705f
Ibrahim Adepoju <ibraheemadepoju@gmail.com>
2022-03-20 13:35:39 +0100

    initialize wordpress

---
+++ ajomoney-woocommerce.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: AjoMoney Gateway For WooCommerce
+ * Plugin URI: https://ajo.money/business
+ * Description: WooCommerce buy now pay later and checkout gateway for AjoMoney
+ * Version: 1.0.0
+ * Author: AjoPay Financial Technology Limited
+ * Author URI: https://ajo.money
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
